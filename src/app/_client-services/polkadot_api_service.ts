5a5fd82ed3242b1b884f7ad89d20e517d6b1ec8d
alphainfinitus <kumar.mridul000@gmail.com>
2024-09-14 05:37:40 +0530

    feat: :sparkles: Polkadot api service class

--- src/app/_client-services/polkadot_api_service.ts
+++ src/app/_client-services/polkadot_api_service.ts
@@ -2,20 +2,60 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-/* eslint-disable no-tabs */
+/* eslint-disable lines-between-class-members */
 
-// import { ENetwork } from '@shared/types';
+import { ClientError } from '@app/_client-utils/clientError';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ERROR_CODES } from '@shared/_constants/errorLiterals';
+import { NETWORKS_DETAILS } from '@shared/_constants/networks';
 
-// class PolkadotApiService {
-// 	private readonly api: ApiPromise;
+import { ENetwork } from '@shared/types';
 
-// 	constructor(private readonly network: ENetwork) {
-// 		this.api = new ApiPromise({
-// 			provider: new WsProvider('wss://rpc.polkadot.io')
-// 		});
-// 	}
+// Usage:
+// const apiService = await PolkadotApiService.Init(ENetwork.POLKADOT);
+// const blockHeight = await apiService.getBlockHeight();
 
-// 	static getPostVotes() {}
-// }
+export class PolkadotApiService {
+	private readonly network: ENetwork;
+	private api: ApiPromise;
+	private currentRpcEndpointIndex: number;
 
-// PolkadotApiService.getPostVotes();
+	private constructor(network: ENetwork, api: ApiPromise) {
+		this.network = network;
+		this.api = api;
+		this.currentRpcEndpointIndex = 0;
+	}
+
+	static async Init(network: ENetwork): Promise<PolkadotApiService> {
+		const api = await ApiPromise.create({
+			provider: new WsProvider(NETWORKS_DETAILS[network as ENetwork].rpcEndpoints[0].url)
+		});
+		return new PolkadotApiService(network, api);
+	}
+
+	getCurrentRpcIndex(): number {
+		return this.currentRpcEndpointIndex;
+	}
+
+	async switchRpcEndpoint(index?: number): Promise<void> {
+		if (index) {
+			// check if valid index
+			if (index < 0 || index >= NETWORKS_DETAILS[this.network].rpcEndpoints.length) {
+				throw new ClientError(ERROR_CODES.CLIENT_ERROR, 'Invalid RPC index');
+			}
+			this.currentRpcEndpointIndex = index;
+		} else {
+			this.currentRpcEndpointIndex = (this.currentRpcEndpointIndex + 1) % NETWORKS_DETAILS[this.network].rpcEndpoints.length;
+		}
+
+		this.api.disconnect();
+		this.api = await ApiPromise.create({
+			provider: new WsProvider(NETWORKS_DETAILS[this.network].rpcEndpoints[this.currentRpcEndpointIndex].url)
+		});
+	}
+
+	async getBlockHeight(): Promise<number> {
+		const header = await this.api.rpc.chain.getHeader();
+		return header.number.toNumber();
+	}
+}
