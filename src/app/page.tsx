d0c52354930122c1bac5ee794cc587bf9c64e4af
Vairamuthu <mvairamuthu2003@gmail.com>
2024-12-01 14:14:13 +0530

    removed the unwanted global scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import Loading from './loading';
-
 export default function Home() {
 	return <div className='text-center leading-10'>Polkassembly</div>;
 }
