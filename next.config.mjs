5ee17801ac2a61694685da99b3cfce6e966b77ce
Vairamuthu <mvairamuthu2003@gmail.com>
2024-12-03 15:18:18 +0530

    added the internalization of next-i18

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	i18n: {
+		locales: ['default', 'en', 'de', 'fr'],
+		defaultLocale: 'default',
+		localeDetection: false,
+		domains: [
+			{
+				domain: 'polkassembly.io',
+				defaultLocale: 'en-US'
+			}
+		]
+	},
+	trailingSlash: true
+};
 
 export default nextConfig;
