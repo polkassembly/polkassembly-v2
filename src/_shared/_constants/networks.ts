bb85e374ff7dbbf5042df72fda7036eb4bf600d3
aadarsh012 <aadarsh012@gmail.com>
2024-12-07 18:58:10 +0530

    added polkadot in NETWORKS_DETAILS

--- src/_shared/_constants/networks.ts
+++ src/_shared/_constants/networks.ts
@@ -22,5 +22,23 @@ export const NETWORKS_DETAILS = {
 			}
 		],
 		chainId: 'polkadot:6408de7737c59c238890533af25896a2'
+	},
+	[ENetwork.POLKADOT]: {
+		disabled: false,
+		key: ENetwork.POLKADOT,
+		name: 'Polkadot',
+		blockTime: 6000,
+		ss58Format: 0,
+		subsquidUrl: 'https://squid.subsquid.io/polkadot-polkassembly/graphql',
+		tokenDecimals: 10,
+		tokenSymbol: 'DOT',
+		blockExplorerUrl: 'https://polkadot.api.subscan.io',
+		rpcEndpoints: [
+			{
+				name: 'via Parity',
+				url: 'wss://rpc.polkadot.io'
+			}
+		],
+		chainId: 'polkadot:6408de7737c59c238890533af25896a2'
 	}
 };
