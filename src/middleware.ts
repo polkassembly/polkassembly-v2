b3e9dfc335b56aeb7e60d6f71cec8935c121bbc2
Vairamuthu <mvairamuthu2003@gmail.com>
2024-12-09 19:07:03 +0530

    updated the es.json for the spanish translation

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-const PUBLIC_FILE = /\.(.*)$/;
-
-export async function middleware(req: NextRequest) {
-	if (req.method === 'OPTIONS') {
-		return new Response('OK', { status: 200 });
-	}
-	if (req.nextUrl.pathname.startsWith('/_next') || req.nextUrl.pathname.includes('/api/') || PUBLIC_FILE.test(req.nextUrl.pathname)) {
-		return NextResponse.next();
-	}
-	if (req.nextUrl.locale === 'default') {
-		const locale = req.cookies.get('NEXT_LOCALE')?.value || 'en';
-
-		return NextResponse.redirect(new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url));
-	}
+export function middleware(request: Request) {
+	if (request.method === 'OPTIONS') return new Response('OK', { status: 200 });
 	return NextResponse.next();
 }
-
-export const config = {
-	matcher: '/api/:path*'
-};
