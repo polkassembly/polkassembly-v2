360a6b7f5f0184f7fcbae232d67d93952a5deaa2
Vairamuthu <mvairamuthu2003@gmail.com>
2024-12-24 15:16:56 +0530

    updated the code based on the pr review

--- src/app/_shared-components/ListingComponent/ListingTab/ListingTab.tsx
+++ src/app/_shared-components/ListingComponent/ListingTab/ListingTab.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-'use client';
-
 import { getPageNumbers } from '@/app/_client-utils/getPageNumber';
 import { IPostListing } from '@/_shared/types';
 import styles from './ListingTab.module.scss';
