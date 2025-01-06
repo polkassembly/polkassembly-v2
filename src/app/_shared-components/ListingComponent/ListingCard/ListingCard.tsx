86897f4f5bc2b4ce0afd9435121747d156d786d5
Vairamuthu <mvairamuthu2003@gmail.com>
2025-01-06 20:08:27 +0530

    updated the listing card

--- src/app/_shared-components/ListingComponent/ListingCard/ListingCard.tsx
+++ src/app/_shared-components/ListingComponent/ListingCard/ListingCard.tsx
@@ -14,61 +14,12 @@ import { getSpanStyle } from '../../TopicTag/TopicTag';
 import styles from './ListingCard.module.scss';
 import StatusTag from '../../StatusTag/StatusTag';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../Tooltip';
+import VotingBar from '../VotingBar/VotingBar';
 
 const calculatePercentage = (count: number, totalCount: number) => {
 	if (totalCount === 0) return 50;
 	return (count / totalCount) * 100;
 };
-
-function HalfCircleProgressBar({ ayePercent, nayPercent }: { ayePercent: number; nayPercent: number }) {
-	const width = 120;
-	const height = 40;
-	const strokeWidth = 16;
-	const radius = 50;
-
-	const createArc = (percentage: number, isAye: boolean) => {
-		const x = isAye ? 10 + (width - 20) * (percentage / 100) : width - 10 - (width - 20) * (percentage / 100);
-		const centerX = width / 2;
-		const y = height - Math.sqrt(radius * radius - (x - centerX) ** 2);
-
-		return `
-            M ${isAye ? 10 : width - 10} ${height}
-            A ${radius} ${radius} 0 0 ${isAye ? 1 : 0}
-            ${x} ${y}
-        `;
-	};
-
-	return (
-		<svg
-			width={width}
-			height={height}
-			viewBox={`0 0 ${width} ${height}`}
-			className={styles.progressBar}
-		>
-			<path
-				d={`M 10 ${height} A ${radius} ${radius} 0 0 1 ${width - 10} ${height}`}
-				fill='none'
-				stroke='#d3d3d3'
-				strokeWidth={strokeWidth}
-			/>
-
-			<path
-				d={createArc(ayePercent, true)}
-				fill='none'
-				stroke='#27d57b'
-				strokeWidth={strokeWidth}
-				strokeLinecap='butt'
-			/>
-			<path
-				d={createArc(nayPercent, false)}
-				fill='none'
-				stroke='#fc3e5d'
-				strokeWidth={strokeWidth}
-				strokeLinecap='butt'
-			/>
-		</svg>
-	);
-}
 
 function ListingCard({
 	title,
@@ -89,8 +40,6 @@ function ListingCard({
 
 	const ayePercent = calculatePercentage(voteMetrics?.aye.count || 0, totalVoteCount);
 	const nayPercent = calculatePercentage(voteMetrics?.nay.count || 0, totalVoteCount);
-
-	console.log(voteMetrics);
 
 	return (
 		<Link
@@ -121,7 +70,7 @@ function ListingCard({
 						<Tooltip>
 							<TooltipTrigger asChild>
 								<div>
-									<HalfCircleProgressBar
+									<VotingBar
 										ayePercent={ayePercent}
 										nayPercent={nayPercent}
 									/>
@@ -131,7 +80,7 @@ function ListingCard({
 								side='top'
 								align='center'
 							>
-								<div className={styles.progressBarText}>
+								<div className={styles.progressBarContainer}>
 									<p>
 										Aye = {formatUSDWithUnits(formatBnBalance(voteMetrics?.aye.value || '0', { numberAfterComma: 2, withThousandDelimitor: false, withUnit: true }, network))} (
 										{ayePercent.toFixed(2)}%)
