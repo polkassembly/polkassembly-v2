e15d21067816944aee3d8f1fcc906ff9c65ef653
Vairamuthu <mvairamuthu2003@gmail.com>
2025-01-14 01:54:34 +0530

    resolved the pr commits

--- src/app/@modal/(.)referenda/[index]/page.tsx
+++ src/app/@modal/(.)referenda/[index]/page.tsx
@@ -2,51 +2,30 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { EProposalType, IPost, IErrorResponse } from '@/_shared/types';
+import { EProposalType, IPost } from '@/_shared/types';
 import { NextApiClientService } from '@/app/_client-services/next_api_client_service';
-import { GetServerSideProps } from 'next';
 import ReferendaDialog from '@ui/ListingComponent/ReferendaDialog';
 
-interface ReferendaPageProps {
-	data: IPost | null;
-	error: IErrorResponse | null;
-	index: string;
+interface PageProps {
+	params: {
+		index: string;
+	};
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { index } = context.params as { index: string };
+export default async function ReferendaPage({ params }: PageProps) {
+	const { index } = params;
+
+	// Fetch data on the server side
 	const result = await NextApiClientService.fetchProposalDetailsApi(EProposalType.REFERENDUM_V2, index);
 
 	if (result.error) {
-		return {
-			props: {
-				data: null,
-				error: result.error,
-				index
-			}
-		};
-	}
-
-	return {
-		props: {
-			data: result.data,
-			error: null,
-			index
-		}
-	};
-};
-
-function Referenda({ data, error, index }: ReferendaPageProps) {
-	if (error || !data) {
-		return <div className='text-center text-text_primary'>{error?.message}</div>;
+		return <div className='text-center text-text_primary'>{result.error.message}</div>;
 	}
 
 	return (
 		<ReferendaDialog
-			data={data}
+			data={result.data as IPost}
 			index={index}
 		/>
 	);
 }
-
-export default Referenda;
