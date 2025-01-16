8647941dc888a698111b19990718195d46fafdca
Vairamuthu <mvairamuthu2003@gmail.com>
2025-01-16 15:18:37 +0530

    updated the styles to module.scss

--- src/app/_shared-components/Dialog/Dialog.tsx
+++ src/app/_shared-components/Dialog/Dialog.tsx
@@ -22,7 +22,7 @@ const DialogOverlay = React.forwardRef<React.ElementRef<typeof DialogPrimitive.O
 	({ className, ...props }, ref) => (
 		<DialogPrimitive.Overlay
 			ref={ref}
-			className={cn(styles.dialogOverlay, className)}
+			className={cn('data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0', className, styles.dialogOverlay)}
 			{...props}
 		/>
 	)
@@ -35,7 +35,11 @@ const DialogContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.C
 			<DialogOverlay />
 			<DialogPrimitive.Content
 				ref={ref}
-				className={cn(styles.dialogContent, className)}
+				className={cn(
+					'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+					className,
+					styles.dialogContent
+				)}
 				{...props}
 			>
 				{children}
