b7d791620c7f0c791073b36c83c6a344be076f80
Vairamuthu <mvairamuthu2003@gmail.com>
2025-01-17 16:32:19 +0530

    updated the post comments

--- src/app/_shared-components/PostComments/PostComments.tsx
+++ src/app/_shared-components/PostComments/PostComments.tsx
@@ -2,11 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-'use client';
-
-import { EProposalType, ICommentResponse } from '@/_shared/types';
+import { EProposalType } from '@/_shared/types';
 import { CommentClientService } from '@/app/_client-services/comment_client_service';
-import { useEffect, useState } from 'react';
 import { useTranslations } from 'next-intl';
 import Comments from './Comments/Comments';
 import classes from './PostComments.module.scss';
@@ -22,42 +19,18 @@ function CommentsTitle({ count }: { count: number }) {
 	);
 }
 
-function PostComments({ proposalType, index }: { proposalType: EProposalType; index: string }) {
-	const [showAllComments, setShowAllComments] = useState(false);
-	const [comments, setComments] = useState<ICommentResponse[]>([]);
-	const [error, setError] = useState<Error | null>(null);
-
-	useEffect(() => {
-		const fetchComments = async () => {
-			const { data, error } = await CommentClientService.getCommentsOfPost({ proposalType, index });
-			if (error) {
-				setError(error);
-			}
-			setComments(data || []);
-		};
-		fetchComments();
-	}, [proposalType, index]);
-
-	const displayedComments = showAllComments ? comments : comments.slice(0, 2);
+async function PostComments({ proposalType, index }: { proposalType: EProposalType; index: string }) {
+	const { data, error } = await CommentClientService.getCommentsOfPost({ proposalType, index });
 
 	return (
 		<div>
 			{error && <p>{error.message}</p>}
-			<CommentsTitle count={comments.length} />
+			<CommentsTitle count={data?.length || 0} />
 			<Comments
 				proposalType={proposalType}
 				index={index}
-				comments={displayedComments}
+				comments={data || []}
 			/>
-			{comments.length > 2 && !showAllComments && (
-				<button
-					onClick={() => setShowAllComments(true)}
-					className='text-pink_primary hover:text-pink_secondary mt-4'
-					type='button'
-				>
-					Load More
-				</button>
-			)}
 		</div>
 	);
 }
