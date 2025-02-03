51f520e36cb02d68f8c743b2d8a2d0ef94d156a9
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-03 11:33:35 +0530

    updated the listing card

--- src/app/_shared-components/ListingComponent/ListingCard/ListingCard.tsx
+++ src/app/_shared-components/ListingComponent/ListingCard/ListingCard.tsx
@@ -20,9 +20,11 @@ import { groupBeneficiariesByAsset } from '@/app/_client-utils/beneficiaryUtils'
 import { calculatePercentage } from '@/app/_client-utils/calculatePercentage';
 import { BN } from '@polkadot/util';
 import { useTheme } from 'next-themes';
-import { MouseEvent } from 'react';
 import Image from 'next/image';
 import DotIcon from '@assets/icons/dot.png';
+import { redirectFromServer } from '@/app/_client-utils/redirectFromServer';
+import { getPostDetailsUrl } from '@/app/_client-utils/getPostDetailsUrl';
+import Link from 'next/link';
 import WestendIcon from '@assets/icons/westend-logo.jpg';
 import DEDIcon from '@assets/icons/ded-asset.png';
 import styles from './ListingCard.module.scss';
@@ -40,7 +42,7 @@ function ListingCard({
 	title: string;
 	onChainInfo: IOnChainPostListing;
 	backgroundColor: string;
-	proposalType: string;
+	proposalType: EProposalType;
 	metrics?: IPostOffChainMetrics;
 	index: number;
 }) {
@@ -65,187 +67,193 @@ function ListingCard({
 	const formattedTime = timeRemaining ? `Deciding ends in ${timeRemaining.days}d : ${timeRemaining.hours}hrs : ${timeRemaining.minutes}mins` : 'Decision period has ended.';
 
 	const groupedByAsset = groupBeneficiariesByAsset(beneficiaries, network);
-
-	const handleFullscreenClick = (e: MouseEvent) => {
-		e.preventDefault();
-		e.stopPropagation();
-		setTimeout(() => {
-			window.location.href = `/referenda/${index}`;
-		}, 100);
-	};
+	const redirectUrl = getPostDetailsUrl(proposalType, index);
 
 	return (
-		<button
-			onClick={handleFullscreenClick}
-			type='button'
-			className={`${styles.listingCard} ${backgroundColor}`}
+		<Link
+			href={redirectUrl}
+			className='w-full'
+			onClick={(e) => {
+				e.stopPropagation();
+				e.preventDefault();
+				redirectFromServer(redirectUrl);
+			}}
 		>
-			<div className='flex items-start lg:gap-4'>
-				<p className={styles.indexText}>#{index}</p>
-				<div className='flex flex-col items-start gap-1'>
-					<h3 className={styles.titleText}>{title}</h3>
-					<div className={styles.infoContainer}>
-						<div className='flex items-center gap-2'>
-							<Address address={proposer} />
-							<span>|</span>
-							<span className={styles.infoItem}>
-								<FaRegClock className={styles.infoIcon} />
-								<span className={styles.infoTimer}>{formattedCreatedAt}</span>
-							</span>
-							{proposalType === EProposalType.DISCUSSION && (
-								<>
-									<span>|</span>
-									<span className={`${getSpanStyle(origin, 1)} ${styles.originStyle}`}>{origin}</span>
-								</>
-							)}
-						</div>
-						<div className='flex items-center gap-2'>
-							<div className={styles.commentContainer}>
-								<span className='hidden lg:block'>|</span>
-								<Icon
-									name='icons/Comment'
-									className={`h-5 w-5 ${theme === ETheme.DARK ? 'dark-icons' : ''}`}
-								/>
-								<span className='text-text_primary'>{metrics?.comments || 0}</span>
+			<div className={`${styles.listingCard} ${backgroundColor}`}>
+				<div className='flex items-start lg:gap-4'>
+					<p className={styles.indexText}>#{index}</p>
+					<div className='flex flex-col items-start gap-1'>
+						<h3 className={styles.titleText}>{title}</h3>
+						<div className={styles.infoContainer}>
+							<div className='flex items-center gap-2'>
+								<Address address={proposer} />
+								<span>|</span>
+								<span className={styles.infoItem}>
+									<FaRegClock className={styles.infoIcon} />
+									<span className={styles.infoTimer}>{formattedCreatedAt}</span>
+								</span>
+								{proposalType === EProposalType.DISCUSSION && (
+									<>
+										<span>|</span>
+										<span className={`${getSpanStyle(origin, 1)} ${styles.originStyle}`}>{origin}</span>
+									</>
+								)}
 							</div>
-							{timeRemaining && (
-								<>
-									<span>|</span>
-									<Tooltip>
-										<TooltipTrigger asChild>
-											<div className='flex items-center gap-1'>
-												<div className='w-8'>
-													<Progress
-														value={decisionPeriodPercentage}
-														className='h-1.5 bg-decision_bar_bg'
+							<div className='flex items-center gap-2'>
+								<div className={styles.commentContainer}>
+									<span className='hidden lg:block'>|</span>
+									<Icon
+										name='icons/Comment'
+										className={`h-5 w-5 ${theme === ETheme.DARK ? 'dark-icons' : ''}`}
+									/>
+									<span className='text-text_primary'>{metrics?.comments || 0}</span>
+								</div>
+								{timeRemaining && (
+									<>
+										<span>|</span>
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<div className='flex items-center gap-1'>
+													<div className='w-8'>
+														<Progress
+															value={decisionPeriodPercentage}
+															className='h-1.5 bg-decision_bar_bg'
+														/>
+													</div>
+												</div>
+											</TooltipTrigger>
+											<TooltipContent
+												side='top'
+												align='center'
+											>
+												<div className={styles.timeBarContainer}>
+													<p>{formattedTime}</p>
+												</div>
+											</TooltipContent>
+										</Tooltip>
+									</>
+								)}
+								{ayePercent > 0 && nayPercent > 0 && (
+									<>
+										<span>|</span>
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<div>
+													<VotingBar
+														ayePercent={ayePercent}
+														nayPercent={nayPercent}
 													/>
 												</div>
-											</div>
-										</TooltipTrigger>
-										<TooltipContent
-											side='top'
-											align='center'
-										>
-											<div className={styles.timeBarContainer}>
-												<p>{formattedTime}</p>
-											</div>
-										</TooltipContent>
-									</Tooltip>
-								</>
-							)}
-							{ayePercent > 0 && nayPercent > 0 && (
-								<>
-									<span>|</span>
-									<Tooltip>
-										<TooltipTrigger asChild>
-											<div>
-												<VotingBar
-													ayePercent={ayePercent}
-													nayPercent={nayPercent}
-												/>
-											</div>
-										</TooltipTrigger>
-										<TooltipContent
-											side='top'
-											align='center'
-										>
-											<div className={styles.progressBarContainer}>
-												<p>
-													Aye = {formatUSDWithUnits(formatBnBalance(voteMetrics?.aye.value || '0', { numberAfterComma: 2, withThousandDelimitor: false, withUnit: true }, network))}{' '}
-													({ayePercent.toFixed(2)}%)
-												</p>
-												<p>
-													Nay = {formatUSDWithUnits(formatBnBalance(voteMetrics?.nay.value || '0', { numberAfterComma: 2, withThousandDelimitor: false, withUnit: true }, network))}{' '}
-													({nayPercent.toFixed(2)}%)
-												</p>
-											</div>
-										</TooltipContent>
-									</Tooltip>
-								</>
-							)}
+											</TooltipTrigger>
+											<TooltipContent
+												side='top'
+												align='center'
+											>
+												<div className={styles.progressBarContainer}>
+													<p>
+														Aye ={' '}
+														{formatUSDWithUnits(formatBnBalance(voteMetrics?.aye.value || '0', { numberAfterComma: 2, withThousandDelimitor: false, withUnit: true }, network))} (
+														{ayePercent.toFixed(2)}%)
+													</p>
+													<p>
+														Nay ={' '}
+														{formatUSDWithUnits(formatBnBalance(voteMetrics?.nay.value || '0', { numberAfterComma: 2, withThousandDelimitor: false, withUnit: true }, network))} (
+														{nayPercent.toFixed(2)}%)
+													</p>
+												</div>
+											</TooltipContent>
+										</Tooltip>
+									</>
+								)}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div className={styles.tagContainer}>
-				{beneficiaries && beneficiaries.length > 0 && groupBeneficiariesByAsset(beneficiaries, network) && (
-					<div className={styles.beneficiaryContainer}>
-						{beneficiaries.length > 1 ? (
-							<Tooltip>
-								<TooltipTrigger asChild>
-									<div className='flex items-center gap-1'>
-										<div className='flex items-center -space-x-1.5'>
-											{Object.entries(groupedByAsset).map(([assetId]) => {
-												const unit = NETWORKS_DETAILS[`${network}`]?.supportedAssets?.[`${assetId}`]?.symbol || NETWORKS_DETAILS[`${network}`]?.tokenSymbol || assetId;
-												const icon = ICONS[unit.toLowerCase() as keyof typeof ICONS] || DotIcon;
-												const isImage = typeof icon === 'object' && icon?.src;
-												return isImage ? (
-													<Image
-														key={assetId}
-														src={icon.src}
-														alt={unit}
-														width={20}
-														height={20}
-														className='h-5 w-5'
-													/>
-												) : (
-													<Icon
-														key={assetId}
-														name={icon as IconName}
-														className='h-5 w-5'
-													/>
+				<div className={styles.tagContainer}>
+					{beneficiaries && beneficiaries.length > 0 && groupBeneficiariesByAsset(beneficiaries, network) && (
+						<div className={styles.beneficiaryContainer}>
+							{beneficiaries.length > 1 ? (
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<div className='flex items-center gap-1'>
+											<div className='flex items-center -space-x-1.5'>
+												{Object.entries(groupedByAsset).map(([assetId]) => {
+													const unit = NETWORKS_DETAILS[`${network}`]?.supportedAssets?.[`${assetId}`]?.symbol || NETWORKS_DETAILS[`${network}`]?.tokenSymbol || assetId;
+													const icon = ICONS[unit.toLowerCase() as keyof typeof ICONS] || DotIcon;
+													const isImage = typeof icon === 'object' && icon?.src;
+													return isImage ? (
+														<Image
+															key={assetId}
+															src={icon.src}
+															alt={unit}
+															width={20}
+															height={20}
+															className='h-5 w-5'
+														/>
+													) : (
+														<Icon
+															key={assetId}
+															name={icon as IconName}
+															className='h-5 w-5'
+														/>
+													);
+												})}
+											</div>
+											<span className='block lg:hidden'>|</span>
+										</div>
+									</TooltipTrigger>
+									<TooltipContent
+										side='top'
+										align='center'
+									>
+										<div className={styles.assetContainer}>
+											{Object.entries(groupedByAsset).map(([assetId, amount]) => {
+												return (
+													<div key={assetId}>
+														~{' '}
+														{formatUSDWithUnits(
+															formatBnBalance(
+																amount.toString(),
+																{ withUnit: true, numberAfterComma: 2 },
+																network,
+																assetId === NETWORKS_DETAILS[`${network}`].tokenSymbol ? null : assetId
+															)
+														)}{' '}
+													</div>
 												);
 											})}
 										</div>
+									</TooltipContent>
+								</Tooltip>
+							) : (
+								Object.entries(groupedByAsset).map(([assetId, amount], i) => (
+									<div
+										className={styles.requestedAmount}
+										key={assetId}
+									>
+										<span className='whitespace-nowrap'>
+											{formatUSDWithUnits(
+												formatBnBalance(
+													amount.toString(),
+													{ withUnit: true, numberAfterComma: 2 },
+													network,
+													assetId === NETWORKS_DETAILS[`${network}`].tokenSymbol ? null : assetId
+												)
+											)}
+										</span>
+										{i < Object.entries(groupedByAsset).length - 1 && <span className='text-text_primary'>&</span>}
 										<span className='block lg:hidden'>|</span>
 									</div>
-								</TooltipTrigger>
-								<TooltipContent
-									side='top'
-									align='center'
-								>
-									<div className={styles.assetContainer}>
-										{Object.entries(groupedByAsset).map(([assetId, amount]) => {
-											return (
-												<div key={assetId}>
-													~{' '}
-													{formatUSDWithUnits(
-														formatBnBalance(
-															amount.toString(),
-															{ withUnit: true, numberAfterComma: 2 },
-															network,
-															assetId === NETWORKS_DETAILS[`${network}`].tokenSymbol ? null : assetId
-														)
-													)}{' '}
-												</div>
-											);
-										})}
-									</div>
-								</TooltipContent>
-							</Tooltip>
-						) : (
-							Object.entries(groupedByAsset).map(([assetId, amount], i) => (
-								<div
-									className={styles.requestedAmount}
-									key={assetId}
-								>
-									<span className='whitespace-nowrap'>
-										{formatUSDWithUnits(
-											formatBnBalance(amount.toString(), { withUnit: true, numberAfterComma: 2 }, network, assetId === NETWORKS_DETAILS[`${network}`].tokenSymbol ? null : assetId)
-										)}
-									</span>
-									{i < Object.entries(groupedByAsset).length - 1 && <span className='text-text_primary'>&</span>}
-									<span className='block lg:hidden'>|</span>
-								</div>
-							))
-						)}
+								))
+							)}
+						</div>
+					)}
+					<div className='flex'>
+						<StatusTag status={status.toLowerCase().replace(/\s+/g, '_')} />
 					</div>
-				)}
-				<div className='flex'>
-					<StatusTag status={status.toLowerCase().replace(/\s+/g, '_')} />
 				</div>
 			</div>
-		</button>
+		</Link>
 	);
 }
 
