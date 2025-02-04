afa6ca345ae166ef4fac197114ac44b91eafe4dd
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-04 14:16:56 +0530

    udpdated the navbar for the mobile

--- src/app/_shared-components/AppLayout/Navbar/Navbar.tsx
+++ src/app/_shared-components/AppLayout/Navbar/Navbar.tsx
@@ -15,6 +15,7 @@ import { setLocaleCookie } from '@/app/_client-utils/setCookieFromServer';
 import { useUserPreferences } from '@/hooks/useUserPreferences';
 import { ChevronDown } from 'lucide-react';
 import { useIsMobile } from '@/hooks/useIsMobile';
+import { FaBars } from 'react-icons/fa';
 import classes from './Navbar.module.scss';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../DropdownMenu';
 import Address from '../../Profile/Address/Address';
@@ -44,80 +45,82 @@ function Navbar() {
 	return (
 		<nav className={classes.navbar}>
 			<p className='pl-8 font-semibold text-navbar_title md:pl-0'>OpenGov</p>
+			{!isMobile ? (
+				<div className='flex items-center gap-x-4'>
+					<Select
+						value={userPreferences.locale}
+						onValueChange={(value: ELocales) => handleLocaleChange(value)}
+					>
+						<SelectTrigger className='w-[180px] border-border_grey bg-network_dropdown_bg'>
+							<SelectValue placeholder='Select Language' />
+						</SelectTrigger>
+						<SelectContent className='border-border_grey'>
+							<div>
+								{Object.entries(LANGUAGES).map(([locale, label]) => (
+									<SelectItem
+										key={locale}
+										value={locale}
+										className='cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800'
+									>
+										{label}
+									</SelectItem>
+								))}
+							</div>
+						</SelectContent>
+					</Select>
 
-			<div className='flex items-center gap-x-4'>
-				<Select
-					value={userPreferences.locale}
-					onValueChange={(value: ELocales) => handleLocaleChange(value)}
-				>
-					<SelectTrigger className='w-[180px] border-border_grey bg-network_dropdown_bg'>
-						<SelectValue placeholder='Select Language' />
-					</SelectTrigger>
-					<SelectContent className='border-border_grey'>
-						<div>
-							{Object.entries(LANGUAGES).map(([locale, label]) => (
-								<SelectItem
-									key={locale}
-									value={locale}
-									className='cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800'
-								>
-									{label}
-								</SelectItem>
-							))}
-						</div>
-					</SelectContent>
-				</Select>
+					<div className='relative'>
+						<NetworkDropdown />
+					</div>
 
-				<div className='relative'>
-					<NetworkDropdown />
-				</div>
-
-				{user?.id ? (
-					<DropdownMenu>
-						<DropdownMenuTrigger>
-							<Button
-								variant='ghost'
-								className='rounded-3xl border border-border_grey bg-wallet_disabled_bg text-sm text-text_primary'
-								size='sm'
-								rightIcon={<ChevronDown size={12} />}
-							>
-								{user.addresses && user.addresses.length > 0 ? (
-									<Address
-										address={user.addresses[0]}
-										walletAddressName={user.username}
-									/>
-								) : (
-									<p>{user.username}</p>
-								)}
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent>
-							<DropdownMenuItem>
-								<Link href='/settings'>{t('Profile.settings')}</Link>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<Link href={`/user/id/${user.id}`}>{t('Profile.profile')}</Link>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
+					{user?.id ? (
+						<DropdownMenu>
+							<DropdownMenuTrigger>
 								<Button
 									variant='ghost'
-									className='p-0'
-									onClick={() => AuthClientService.logout(() => setUser(null))}
+									className='rounded-3xl border border-border_grey bg-wallet_disabled_bg text-sm text-text_primary'
+									size='sm'
+									rightIcon={<ChevronDown size={12} />}
 								>
-									{t('Profile.logout')}
+									{user.addresses && user.addresses.length > 0 ? (
+										<Address
+											address={user.addresses[0]}
+											walletAddressName={user.username}
+										/>
+									) : (
+										<p>{user.username}</p>
+									)}
 								</Button>
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				) : (
-					<Link href='/login'>
-						<Button>{t('Profile.login')}</Button>
-					</Link>
-				)}
-
-				<RPCSwitchDropdown className='hidden lg:flex' />
-				<ToggleButton className='hidden lg:flex' />
-			</div>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuItem>
+									<Link href='/settings'>{t('Profile.settings')}</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<Link href={`/user/id/${user.id}`}>{t('Profile.profile')}</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<Button
+										variant='ghost'
+										className='p-0'
+										onClick={() => AuthClientService.logout(() => setUser(null))}
+									>
+										{t('Profile.logout')}
+									</Button>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					) : (
+						<Link href='/login'>
+							<Button>{t('Profile.login')}</Button>
+						</Link>
+					)}
+					<RPCSwitchDropdown />
+					<ToggleButton />
+				</div>
+			) : (
+				<FaBars />
+			)}
 		</nav>
 	);
 }
