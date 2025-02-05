16e5a85f1eb0900f82f77ce9a2dc000f58d2c95c
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-05 19:52:22 +0530

    fixes

--- src/app/_shared-components/Leaderboard/index.tsx
+++ src/app/_shared-components/Leaderboard/index.tsx
@@ -5,10 +5,36 @@
 import { IGenericListingResponse, IPublicUser } from '@/_shared/types';
 import { dayjs } from '@/_shared/_utils/dayjsInit';
 import React from 'react';
+import Card from '@assets/leaderboard/card.svg';
+import Cup from '@assets/leaderboard/cup.svg';
+import Image from 'next/image';
 
 function Leaderboard({ data }: { data: IGenericListingResponse<IPublicUser> }) {
 	return (
 		<div className='bg-page_background px-8 pt-6 lg:px-12'>
+			<div className='relative w-full'>
+				<Image
+					src={Card}
+					alt='Leaderboard'
+					className='h-auto w-full'
+					width={100}
+					height={100}
+				/>
+				<div className='absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center'>
+					<Image
+						src={Cup}
+						alt='Cup'
+						className='h-56 w-56'
+						width={100}
+						height={100}
+					/>
+					<div className='flex flex-col gap-2'>
+						<p className='text-2xl font-bold text-white'>Leaderboard</p>
+						<p className='text-sm text-white'>Find your rank in the ecosystem</p>
+					</div>
+				</div>
+			</div>
+
 			{data.items.map((item) => {
 				// eslint-disable-next-line
 				const createdAtSeconds = item.createdAt?._seconds;
