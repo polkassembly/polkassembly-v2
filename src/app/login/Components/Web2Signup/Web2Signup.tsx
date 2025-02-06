8338d6ce739d7aee5c156987c843f4f6d7573250
Vairamuthu <88650559+vmmuthu31@users.noreply.github.com>
2025-02-06 22:12:57 +0700

    Update Web2Signup.tsx

--- src/app/login/Components/Web2Signup/Web2Signup.tsx
+++ src/app/login/Components/Web2Signup/Web2Signup.tsx
@@ -1,7 +1,6 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import { Input } from '@ui/Input';
 import { Button } from '@ui/Button';
 import React, { useState } from 'react';
