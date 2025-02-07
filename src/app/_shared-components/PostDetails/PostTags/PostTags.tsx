5476e8881296bccae5bf716f9ccf0ffdea69fea6
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-07 17:01:03 +0530

    pushing the fixes for the modal

--- src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
+++ src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
@@ -1,25 +1,20 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import React from 'react';
+import { ITag } from '@/_shared/types';
 import classes from './PostTags.module.scss';
 
-function PostTags({ tags }: { tags: string[] }) {
-	if (!tags?.length) return null;
-
+function PostTags({ tags }: { tags: ITag[] }) {
 	return (
-		<div
-			className='flex items-center gap-x-2'
-			aria-label='Post Tags'
-		>
+		<div className='flex items-center gap-x-2'>
 			{tags.map((tag) => (
-				<span
-					key={tag}
+				<div
+					key={tag.value}
 					className={classes.tag}
 				>
-					{tag}
-				</span>
+					{tag.value}
+				</div>
 			))}
 		</div>
 	);
