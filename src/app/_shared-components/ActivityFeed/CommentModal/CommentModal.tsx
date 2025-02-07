30d605a2227b6566c000cff16ecca812132fbf9b
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-07 11:18:52 +0530

    pushing the fixes

--- src/app/_shared-components/ActivityFeed/CommentModal/CommentModal.tsx
+++ src/app/_shared-components/ActivityFeed/CommentModal/CommentModal.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-'use client';
-
 import React, { useRef, useState } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@ui/Dialog/Dialog';
 import Image from 'next/image';
--- src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/VoteHistoryTable.tsx
+++ src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/VoteHistoryTable.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-'use client';
-
 import { IVoteData } from '@/_shared/types';
 import { ColumnDef, SortingState, flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
 import React, { useState } from 'react';
