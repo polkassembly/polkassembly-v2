b1ae1b048b1aa5101d64731777d74c2209514e9d
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-07 14:29:12 +0530

    pushing the fixes

---
+++ src/app/delegation/page.tsx
@@ -0,0 +1,16 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import React from 'react';
+import Delegation from '@ui/Delegation/index';
+
+function DelegationPage() {
+	return (
+		<div className='grid grid-cols-1 gap-5 p-5 sm:p-10'>
+			<Delegation />
+		</div>
+	);
+}
+
+export default DelegationPage;
