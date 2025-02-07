3b1c7c368bb33780357c4f12bc6561fbd7412660
Vairamuthu <mvairamuthu2003@gmail.com>
2025-02-07 11:21:03 +0530

    pushing the fixes

--- src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/VoteHistoryTable.tsx
+++ src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/VoteHistoryTable.tsx
@@ -1,7 +1,6 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import { IVoteData } from '@/_shared/types';
 import { ColumnDef, SortingState, flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
 import React, { useState } from 'react';
