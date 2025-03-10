ffa39c4a122d8c46767345bd451f1bcd304fa0d9
alphainfinitus <kumar.mridul000@gmail.com>
2025-03-10 12:26:14 +0530

    feat: Add reactions to comments from multiple sources

--- src/_shared/types.ts
+++ src/_shared/types.ts
@@ -592,6 +592,7 @@ export interface IComment {
 export interface ICommentResponse extends IComment {
 	user: Omit<IPublicUser, 'rank'>;
 	children?: ICommentResponse[];
+	reactions?: IReaction[];
 }
 
 export interface IOnChainIdentity {
