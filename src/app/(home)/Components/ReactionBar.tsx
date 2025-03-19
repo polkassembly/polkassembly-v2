542262e3145d76ba8ec9aa3c73849c583049983d
Vairamuthu <mvairamuthu2003@gmail.com>
2025-03-19 11:43:53 +0530

    fix: update share message format in ReactionBar and PostActions components

--- src/app/(home)/Components/ReactionBar.tsx
+++ src/app/(home)/Components/ReactionBar.tsx
@@ -64,7 +64,7 @@ function ReactionBar({
 
 	const handleShare = () => {
 		const titlePart = postData?.title ? ` for ${postData.title}` : '';
-		const message = `The referendum${titlePart} is now live for @Polkassembly \nCast your vote here: ${global?.window?.location?.href}`;
+		const message = `The referendum "${titlePart}" is now live for @Polkassembly \nCast your vote here: ${global?.window?.location?.href}`;
 		const twitterParameters = [`text=${encodeURIComponent(message)}`, `via=${encodeURIComponent('polk_gov')}`];
 		const url = `https://twitter.com/intent/tweet?${twitterParameters.join('&')}`;
 		global?.window?.open(url);
