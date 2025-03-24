4162966ec5b627816e94a0f77481ec5a4a85c9e0
alphainfinitus <kumar.mridul000@gmail.com>
2025-03-25 03:27:54 +0530

    feat: update network track details and enhance treasury management
    - Added new treasury spending origins with updated parameters for decisionDeposit, preparePeriod, decisionPeriod, confirmPeriod, and minEnactmentPeriod to improve treasury management.
    - Adjusted decisionDeposit values for existing origins to align with new treasury strategies.
    - Included a TODO comment to indicate future updates for networks beyond Polkadot.

--- src/_shared/_constants/networks.ts
+++ src/_shared/_constants/networks.ts
@@ -182,6 +182,7 @@ const NETWORK_TOKEN_DECIMALS: Record<ENetwork, number> = {
 	[ENetwork.WESTEND]: 12
 } as const;
 
+// TODO: update for other networks than polkadot
 const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrackInfo>>> = {
 	[ENetwork.POLKADOT]: {
 		[EPostOrigin.ROOT]: {
@@ -1296,171 +1297,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Origin',
 			name: 'root',
 			maxDeciding: 1,
-			decisionDeposit: new BN('100000000000000'),
+			decisionDeposit: new BN('100000000000000000'),
 			preparePeriod: 80,
 			decisionPeriod: 200,
 			confirmPeriod: 120,
 			minEnactmentPeriod: 50,
-			minApproval: {
-				reciprocal: {
-					factor: 222222224,
-					xOffset: 333333335,
-					yOffset: 333333332
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.BIG_SPENDER]: {
-			trackId: 34,
-			description: 'Origin able to spend up to 1,000,000 WND from the treasury at once',
-			group: 'Treasury',
-			name: 'big_spender',
-			maxSpend: new BN('1000000').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.WESTEND]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('4000000000000'),
-			preparePeriod: 2400,
-			decisionPeriod: 403200,
-			confirmPeriod: 28800,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 28326977,
-					xOffset: 53763445,
-					yOffset: -26881723
-				}
-			}
-		},
-		[EPostOrigin.MEDIUM_SPENDER]: {
-			trackId: 33,
-			description: 'Origin able to spend up to 100,000 WND from the treasury at once',
-			group: 'Treasury',
-			name: 'medium_spender',
-			maxSpend: new BN('100000').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.WESTEND]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('2000000000000'),
-			preparePeriod: 2400,
-			decisionPeriod: 403200,
-			confirmPeriod: 14400,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 821428571,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 14377233,
-					xOffset: 27972031,
-					yOffset: -13986016
-				}
-			}
-		},
-		[EPostOrigin.SMALL_SPENDER]: {
-			trackId: 32,
-			description: 'Origin able to spend up to 10,000 WND from the treasury at once',
-			group: 'Treasury',
-			name: 'small_spender',
-			maxSpend: new BN('10000').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.WESTEND]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('1000000000000'),
-			preparePeriod: 2400,
-			decisionPeriod: 403200,
-			confirmPeriod: 7200,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 607142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 7892829,
-					xOffset: 15544040,
-					yOffset: -7772020
-				}
-			}
-		},
-		[EPostOrigin.BIG_TIPPER]: {
-			trackId: 31,
-			description: 'Origin able to spend up to 1000 WND from the treasury at once',
-			group: 'Treasury',
-			name: 'big_tipper',
-			maxSpend: new BN('1000').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.WESTEND]))),
-			maxDeciding: 100,
-			decisionDeposit: new BN('100000000000'),
-			preparePeriod: 100,
-			decisionPeriod: 100800,
-			confirmPeriod: 600,
-			minEnactmentPeriod: 100,
-			minApproval: {
-				linearDecreasing: {
-					length: 357142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 4149097,
-					xOffset: 8230453,
-					yOffset: -4115227
-				}
-			}
-		},
-		[EPostOrigin.SMALL_TIPPER]: {
-			trackId: 30,
-			description: 'Origin able to spend up to 250 WND from the treasury at once',
-			group: 'Treasury',
-			name: 'small_tipper',
-			maxDeciding: 200,
-			decisionDeposit: new BN('30000000000'),
-			preparePeriod: 10,
-			decisionPeriod: 140,
-			confirmPeriod: 40,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 357142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 1620729,
-					xOffset: 3231018,
-					yOffset: -1615509
-				}
-			}
-		},
-		[EPostOrigin.TREASURER]: {
-			trackId: 11,
-			description: TREASURER_DESCRIPTION,
-			group: 'Origin',
-			name: 'treasurer',
-			maxDeciding: 10,
-			decisionDeposit: new BN('10000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 100800,
-			minEnactmentPeriod: 14400,
 			minApproval: {
 				reciprocal: {
 					factor: 222222224,
@@ -1508,11 +1349,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Main',
 			name: 'staking_admin',
 			maxDeciding: 10,
-			decisionDeposit: new BN('50000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('5000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				linearDecreasing: {
 					length: 607142857,
@@ -1528,17 +1369,43 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 				}
 			}
 		},
+		[EPostOrigin.TREASURER]: {
+			trackId: 11,
+			description: TREASURER_DESCRIPTION,
+			group: 'Origin',
+			name: 'treasurer',
+			maxDeciding: 10,
+			decisionDeposit: new BN('1000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 50,
+			minApproval: {
+				reciprocal: {
+					factor: 222222224,
+					xOffset: 333333335,
+					yOffset: 333333332
+				}
+			},
+			minSupport: {
+				linearDecreasing: {
+					length: 1000000000,
+					floor: 0,
+					ceil: 500000000
+				}
+			}
+		},
 		[EPostOrigin.LEASE_ADMIN]: {
 			trackId: 12,
 			description: LEASE_ADMIN_DESCRIPTION,
 			group: 'Main',
 			name: 'lease_admin',
 			maxDeciding: 10,
-			decisionDeposit: new BN('50000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('5000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				linearDecreasing: {
 					length: 607142857,
@@ -1560,11 +1427,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Origin',
 			name: 'fellowship_admin',
 			maxDeciding: 10,
-			decisionDeposit: new BN('50000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('5000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				linearDecreasing: {
 					length: 607142857,
@@ -1586,11 +1453,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Main',
 			name: 'general_admin',
 			maxDeciding: 10,
-			decisionDeposit: new BN('50000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('5000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				reciprocal: {
 					factor: 222222224,
@@ -1612,11 +1479,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Main',
 			name: 'auction_admin',
 			maxDeciding: 10,
-			decisionDeposit: new BN('50000000000000'),
-			preparePeriod: 1200,
-			decisionPeriod: 403200,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('5000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				reciprocal: {
 					factor: 222222224,
@@ -1638,11 +1505,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Governance',
 			name: 'referendum_canceller',
 			maxDeciding: 1000,
-			decisionDeposit: new BN('333333333330000'),
-			preparePeriod: 1200,
-			decisionPeriod: 100800,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('10000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 140,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				linearDecreasing: {
 					length: 607142857,
@@ -1664,11 +1531,11 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 			group: 'Governance',
 			name: 'referendum_killer',
 			maxDeciding: 1000,
-			decisionDeposit: new BN('1666666666650000'),
-			preparePeriod: 1200,
-			decisionPeriod: 201600,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 100,
+			decisionDeposit: new BN('50000000000000000'),
+			preparePeriod: 80,
+			decisionPeriod: 200,
+			confirmPeriod: 80,
+			minEnactmentPeriod: 30,
 			minApproval: {
 				linearDecreasing: {
 					length: 607142857,
@@ -1681,6 +1548,136 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 					factor: 7892829,
 					xOffset: 15544040,
 					yOffset: -7772020
+				}
+			}
+		},
+		[EPostOrigin.SMALL_TIPPER]: {
+			trackId: 30,
+			description: 'Origin able to spend up to 250 WND from the treasury at once',
+			group: 'Treasury',
+			name: 'small_tipper',
+			maxDeciding: 200,
+			decisionDeposit: new BN('30000000000'),
+			preparePeriod: 10,
+			decisionPeriod: 140,
+			confirmPeriod: 40,
+			minEnactmentPeriod: 10,
+			minApproval: {
+				linearDecreasing: {
+					length: 357142857,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 1620729,
+					xOffset: 3231018,
+					yOffset: -1615509
+				}
+			}
+		},
+		[EPostOrigin.BIG_TIPPER]: {
+			trackId: 31,
+			description: 'Origin able to spend up to 1000 WND from the treasury at once',
+			group: 'Treasury',
+			name: 'big_tipper',
+			maxDeciding: 100,
+			decisionDeposit: new BN('300000000000'),
+			preparePeriod: 40,
+			decisionPeriod: 140,
+			confirmPeriod: 120,
+			minEnactmentPeriod: 30,
+			minApproval: {
+				linearDecreasing: {
+					length: 357142857,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 4149097,
+					xOffset: 8230453,
+					yOffset: -4115227
+				}
+			}
+		},
+		[EPostOrigin.SMALL_SPENDER]: {
+			trackId: 32,
+			description: 'Origin able to spend up to 10,000 WND from the treasury at once',
+			group: 'Treasury',
+			name: 'small_spender',
+			maxDeciding: 50,
+			decisionDeposit: new BN('3000000000000'),
+			preparePeriod: 100,
+			decisionPeriod: 200,
+			confirmPeriod: 100,
+			minEnactmentPeriod: 50,
+			minApproval: {
+				linearDecreasing: {
+					length: 607142857,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 7892829,
+					xOffset: 15544040,
+					yOffset: -7772020
+				}
+			}
+		},
+		[EPostOrigin.MEDIUM_SPENDER]: {
+			trackId: 33,
+			description: 'Origin able to spend up to 100,000 WND from the treasury at once',
+			group: 'Treasury',
+			name: 'medium_spender',
+			maxDeciding: 50,
+			decisionDeposit: new BN('6000000000000'),
+			preparePeriod: 100,
+			decisionPeriod: 200,
+			confirmPeriod: 120,
+			minEnactmentPeriod: 50,
+			minApproval: {
+				linearDecreasing: {
+					length: 821428571,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 14377233,
+					xOffset: 27972031,
+					yOffset: -13986016
+				}
+			}
+		},
+		[EPostOrigin.BIG_SPENDER]: {
+			trackId: 34,
+			description: 'Origin able to spend up to 1,000,000 WND from the treasury at once',
+			group: 'Treasury',
+			name: 'big_spender',
+			maxDeciding: 50,
+			decisionDeposit: new BN('12000000000000'),
+			preparePeriod: 100,
+			decisionPeriod: 200,
+			confirmPeriod: 140,
+			minEnactmentPeriod: 50,
+			minApproval: {
+				linearDecreasing: {
+					length: 1000000000,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 28326977,
+					xOffset: 53763445,
+					yOffset: -26881723
 				}
 			}
 		}
