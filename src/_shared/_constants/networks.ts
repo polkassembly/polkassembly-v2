912e64e08d2d81a4b8ef4809b4e36c51ccccd217
alphainfinitus <kumar.mridul000@gmail.com>
2025-03-25 03:44:48 +0530

    feat: update network track details with new treasury origins and parameters
    - Added new treasury origins: whitelisted_caller, big_spender, medium_spender, small_spender, and big_tipper with detailed parameters for maxSpend, decisionDeposit, preparePeriod, decisionPeriod, confirmPeriod, and minEnactmentPeriod.
    - Adjusted existing parameters for staking_admin and small_tipper to enhance treasury management and decision-making capabilities.

--- src/_shared/_constants/networks.ts
+++ src/_shared/_constants/networks.ts
@@ -635,193 +635,6 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 				}
 			}
 		},
-		[EPostOrigin.WISH_FOR_CHANGE]: {
-			trackId: 2,
-			description: WISH_FOR_CHANGE_DESCRIPTION,
-			group: 'Main',
-			name: 'wish_for_change',
-			maxDeciding: 10,
-			decisionDeposit: new BN('666666666660000'),
-			preparePeriod: 1200,
-			decisionPeriod: 201600,
-			confirmPeriod: 14400,
-			minEnactmentPeriod: 100,
-			minApproval: {
-				reciprocal: {
-					factor: 222222224,
-					xOffset: 333333335,
-					yOffset: 333333332
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.BIG_SPENDER]: {
-			trackId: 34,
-			description: 'Origin able to spend up to 33,333 KSM from the treasury at once.',
-			group: 'Treasury',
-			name: 'big_spender',
-			maxSpend: new BN('33333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('13333333333200'),
-			preparePeriod: 2400,
-			decisionPeriod: 201600,
-			confirmPeriod: 28800,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 28326977,
-					xOffset: 53763445,
-					yOffset: -26881723
-				}
-			}
-		},
-		[EPostOrigin.MEDIUM_SPENDER]: {
-			trackId: 33,
-			description: 'Origin able to spend up to 3,333 KSM from the treasury at once.',
-			group: 'Treasury',
-			name: 'medium_spender',
-			maxSpend: new BN('3333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('6666666666600'),
-			preparePeriod: 2400,
-			decisionPeriod: 201600,
-			confirmPeriod: 14400,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 821428571,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 14377233,
-					xOffset: 27972031,
-					yOffset: -13986016
-				}
-			}
-		},
-		[EPostOrigin.SMALL_SPENDER]: {
-			trackId: 32,
-			description: 'Origin able to spend up to 333 KSM from the treasury at once.',
-			group: 'Treasury',
-			name: 'small_spender',
-			maxSpend: new BN('333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
-			maxDeciding: 50,
-			decisionDeposit: new BN('3333333333300'),
-			preparePeriod: 2400,
-			decisionPeriod: 201600,
-			confirmPeriod: 7200,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				linearDecreasing: {
-					length: 607142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 7892829,
-					xOffset: 15544040,
-					yOffset: -7772020
-				}
-			}
-		},
-		[EPostOrigin.BIG_TIPPER]: {
-			trackId: 31,
-			description: 'Origin able to spend up to 5 KSM from the treasury at once.',
-			group: 'Treasury',
-			name: 'big_tipper',
-			maxSpend: new BN('5').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
-			maxDeciding: 100,
-			decisionDeposit: new BN('333333333330'),
-			preparePeriod: 100,
-			decisionPeriod: 100800,
-			confirmPeriod: 600,
-			minEnactmentPeriod: 100,
-			minApproval: {
-				linearDecreasing: {
-					length: 357142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 4149097,
-					xOffset: 8230453,
-					yOffset: -4115227
-				}
-			}
-		},
-		[EPostOrigin.SMALL_TIPPER]: {
-			trackId: 30,
-			description: 'Origin able to spend up to 1 KSM from the treasury at once.',
-			group: 'Treasury',
-			name: 'small_tipper',
-			maxDeciding: 200,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 10,
-			decisionPeriod: 100800,
-			confirmPeriod: 100,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 357142857,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				reciprocal: {
-					factor: 1620729,
-					xOffset: 3231018,
-					yOffset: -1615509
-				}
-			}
-		},
-		[EPostOrigin.TREASURER]: {
-			trackId: 11,
-			description: TREASURER_DESCRIPTION,
-			group: 'Treasury',
-			name: 'treasurer',
-			maxSpend: new BN('333333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333000'),
-			preparePeriod: 1200,
-			decisionPeriod: 201600,
-			confirmPeriod: 1800,
-			minEnactmentPeriod: 14400,
-			minApproval: {
-				reciprocal: {
-					factor: 222222224,
-					xOffset: 333333335,
-					yOffset: 333333332
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
 		[EPostOrigin.WHITELISTED_CALLER]: {
 			trackId: 1,
 			description: WHITELISTED_CALLER_DESCRIPTION,
@@ -848,6 +661,32 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 				}
 			}
 		},
+		[EPostOrigin.WISH_FOR_CHANGE]: {
+			trackId: 2,
+			description: WISH_FOR_CHANGE_DESCRIPTION,
+			group: 'Main',
+			name: 'wish_for_change',
+			maxDeciding: 10,
+			decisionDeposit: new BN('666666666660000'),
+			preparePeriod: 1200,
+			decisionPeriod: 201600,
+			confirmPeriod: 14400,
+			minEnactmentPeriod: 100,
+			minApproval: {
+				reciprocal: {
+					factor: 222222224,
+					xOffset: 333333335,
+					yOffset: 333333332
+				}
+			},
+			minSupport: {
+				linearDecreasing: {
+					length: 1000000000,
+					floor: 0,
+					ceil: 500000000
+				}
+			}
+		},
 		[EPostOrigin.STAKING_ADMIN]: {
 			trackId: 10,
 			description: STAKING_ADMIN_DESCRIPTION,
@@ -871,6 +710,33 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 					factor: 7892829,
 					xOffset: 15544040,
 					yOffset: -7772020
+				}
+			}
+		},
+		[EPostOrigin.TREASURER]: {
+			trackId: 11,
+			description: TREASURER_DESCRIPTION,
+			group: 'Treasury',
+			name: 'treasurer',
+			maxSpend: new BN('333333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 10,
+			decisionDeposit: new BN('33333333333000'),
+			preparePeriod: 1200,
+			decisionPeriod: 201600,
+			confirmPeriod: 28800,
+			minEnactmentPeriod: 14400,
+			minApproval: {
+				reciprocal: {
+					factor: 222222224,
+					xOffset: 333333335,
+					yOffset: 333333332
+				}
+			},
+			minSupport: {
+				linearDecreasing: {
+					length: 1000000000,
+					floor: 0,
+					ceil: 500000000
 				}
 			}
 		},
@@ -1030,69 +896,126 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 				}
 			}
 		},
-		[EPostOrigin.CANDIDATES]: {
-			fellowshipOrigin: true,
-			description: 'Origin commanded by any members of the Polkadot Fellowship (no Dan grade needed)',
-			trackId: 0,
-			name: 'candidates',
-			maxDeciding: 10,
+		[EPostOrigin.SMALL_TIPPER]: {
+			trackId: 30,
+			description: 'Origin able to spend up to 1 KSM from the treasury at once.',
+			group: 'Treasury',
+			name: 'small_tipper',
+			maxSpend: new BN('1').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 200,
+			decisionDeposit: new BN('33333333333'),
+			preparePeriod: 10,
+			decisionPeriod: 100800,
+			confirmPeriod: 100,
+			minEnactmentPeriod: 10,
+			minApproval: {
+				linearDecreasing: {
+					length: 357142857,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 1620729,
+					xOffset: 3231018,
+					yOffset: -1615509
+				}
+			}
+		},
+		[EPostOrigin.BIG_TIPPER]: {
+			trackId: 31,
+			description: 'Origin able to spend up to 5 KSM from the treasury at once.',
+			group: 'Treasury',
+			name: 'big_tipper',
+			maxSpend: new BN('5').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 100,
+			decisionDeposit: new BN('333333333330'),
+			preparePeriod: 100,
+			decisionPeriod: 100800,
+			confirmPeriod: 600,
+			minEnactmentPeriod: 100,
+			minApproval: {
+				linearDecreasing: {
+					length: 357142857,
+					floor: 500000000,
+					ceil: 1000000000
+				}
+			},
+			minSupport: {
+				reciprocal: {
+					factor: 4149097,
+					xOffset: 8230453,
+					yOffset: -4115227
+				}
+			}
+		},
+		[EPostOrigin.SMALL_SPENDER]: {
+			trackId: 32,
+			description: 'Origin able to spend up to 333 KSM from the treasury at once.',
+			group: 'Treasury',
+			name: 'small_spender',
+			maxSpend: new BN('333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 50,
 			decisionDeposit: new BN('3333333333300'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
+			preparePeriod: 2400,
+			decisionPeriod: 201600,
+			confirmPeriod: 7200,
+			minEnactmentPeriod: 14400,
 			minApproval: {
 				linearDecreasing: {
-					length: 1000000000,
+					length: 607142857,
 					floor: 500000000,
 					ceil: 1000000000
 				}
 			},
 			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
+				reciprocal: {
+					factor: 7892829,
+					xOffset: 15544040,
+					yOffset: -7772020
 				}
 			}
 		},
-		[EPostOrigin.MEMBERS]: {
-			fellowshipOrigin: true,
-			trackId: 1,
-			description: 'Origin commanded by rank 1 of the Polkadot Fellowship and with a success of 1',
-			name: 'members',
-			maxDeciding: 10,
-			decisionDeposit: new BN('333333333330'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
+		[EPostOrigin.MEDIUM_SPENDER]: {
+			trackId: 33,
+			description: 'Origin able to spend up to 3,333 KSM from the treasury at once.',
+			group: 'Treasury',
+			name: 'medium_spender',
+			maxSpend: new BN('3333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 50,
+			decisionDeposit: new BN('6666666666600'),
+			preparePeriod: 2400,
+			decisionPeriod: 201600,
+			confirmPeriod: 14400,
+			minEnactmentPeriod: 14400,
 			minApproval: {
 				linearDecreasing: {
-					length: 1000000000,
+					length: 821428571,
 					floor: 500000000,
 					ceil: 1000000000
 				}
 			},
 			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
+				reciprocal: {
+					factor: 14377233,
+					xOffset: 27972031,
+					yOffset: -13986016
 				}
 			}
 		},
-		[EPostOrigin.PROFICIENTS]: {
-			fellowshipOrigin: true,
-			trackId: 2,
-			description: 'Origin commanded by rank 2 of the Polkadot Fellowship and with a success of 2',
-			name: 'proficients',
-			maxDeciding: 10,
-			decisionDeposit: new BN('333333333330'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
+		[EPostOrigin.BIG_SPENDER]: {
+			trackId: 34,
+			description: 'Origin able to spend up to 33,333 KSM from the treasury at once.',
+			group: 'Treasury',
+			name: 'big_spender',
+			maxSpend: new BN('33333').mul(new BN(10).pow(new BN(NETWORK_TOKEN_DECIMALS[ENetwork.KUSAMA]))),
+			maxDeciding: 50,
+			decisionDeposit: new BN('13333333333200'),
+			preparePeriod: 2400,
+			decisionPeriod: 201600,
+			confirmPeriod: 28800,
+			minEnactmentPeriod: 14400,
 			minApproval: {
 				linearDecreasing: {
 					length: 1000000000,
@@ -1101,192 +1024,10 @@ const NETWORK_TRACK_DETAILS: Record<ENetwork, Partial<Record<EPostOrigin, ITrack
 				}
 			},
 			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.FELLOWS]: {
-			fellowshipOrigin: true,
-			trackId: 3,
-			description: 'Origin commanded by Polkadot Fellows (3rd Dan fellows or greater)',
-			name: 'fellows',
-			maxDeciding: 10,
-			decisionDeposit: new BN('333333333330'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.SENIOR_FELLOWS]: {
-			fellowshipOrigin: true,
-			trackId: 4,
-			description: 'Origin commanded by rank 4 of the Polkadot Fellowship and with a success of 4',
-			name: 'senior fellows',
-			maxDeciding: 10,
-			decisionDeposit: new BN('333333333330'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.EXPERTS]: {
-			fellowshipOrigin: true,
-			trackId: 5,
-			description: 'Origin commanded by Polkadot Experts (5th Dan fellows or greater)',
-			name: 'experts',
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.SENIOR_EXPERTS]: {
-			fellowshipOrigin: true,
-			trackId: 6,
-			description: 'Origin commanded by rank 6 of the Polkadot Fellowship and with a success of 6',
-			name: 'senior experts',
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.MASTERS]: {
-			fellowshipOrigin: true,
-			trackId: 7,
-			description: 'Origin commanded by Polkadot Masters (7th Dan fellows of greater)',
-			name: 'masters',
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.SENIOR_MASTERS]: {
-			fellowshipOrigin: true,
-			trackId: 8,
-			description: 'Origin commanded by rank 8 of the Polkadot Fellowship and with a success of 8',
-			name: 'senior masters',
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
-				}
-			}
-		},
-		[EPostOrigin.GRAND_MASTERS]: {
-			fellowshipOrigin: true,
-			trackId: 9,
-			description: 'Origin commanded by rank 9 of the Polkadot Fellowship and with a success of 9',
-			name: 'grand masters',
-			maxDeciding: 10,
-			decisionDeposit: new BN('33333333333'),
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 300,
-			minEnactmentPeriod: 10,
-			minApproval: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 500000000,
-					ceil: 1000000000
-				}
-			},
-			minSupport: {
-				linearDecreasing: {
-					length: 1000000000,
-					floor: 0,
-					ceil: 500000000
+				reciprocal: {
+					factor: 28326977,
+					xOffset: 53763445,
+					yOffset: -26881723
 				}
 			}
 		}
