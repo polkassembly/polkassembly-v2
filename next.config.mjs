36f7cb332b59037342dec867e938186482f75b83
alphainfinitus <kumar.mridul000@gmail.com>
2025-04-03 19:33:44 +0530

    feat: Add CORS and security headers for API routes
    - Implemented custom headers for all v1 API routes to enhance security and manage cross-origin requests.
    - Included headers for CORS, X-Frame-Options, X-XSS-Protection, Content-Security-Policy, and Cache-Control.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,25 @@ const withNextIntl = createNextIntlPlugin('./src/intl/intlRequest.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async headers() {
+		return [
+			{
+				// matching all v1 API routes
+				source: '/api/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: '*' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+					{ key: 'Access-Control-Allow-Headers', value: '*' },
+					{ key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+					{ key: 'X-XSS-Protection', value: '1; mode=block' },
+					{ key: 'X-Content-Type-Options', value: 'nosniff' },
+					{ key: 'Content-Security-Policy', value: "default-src 'self'; img-src '*' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';" },
+					{ key: 'Cache-Control', value: 's-maxage=60, stale-while-revalidate=59' }
+				]
+			}
+		];
+	},
 	images: {
 		remotePatterns: [
 			{
