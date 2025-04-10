de04bc22a4662bf8eabbf0a500550e3cbeed2cf3
Jasmeet <jasmeetsonu990@gmail.com>
2025-04-10 14:40:22 +0530

    fixes

--- src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/DelegatedVotesDropdown/DelegatedVotesDropdown.tsx
+++ src/app/_shared-components/PostDetails/VoteSummary/VoteHistory/DelegatedVotesDropdown/DelegatedVotesDropdown.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React from 'react';
+import React, { useState } from 'react';
 import { formatBnBalance } from '@/app/_client-utils/formatBnBalance';
 import { dayjs } from '@/_shared/_utils/dayjsInit';
 import { NETWORKS_DETAILS } from '@/_shared/_constants/networks';
@@ -17,6 +17,8 @@ function DelegatedVotesDropdown({ voteData, voterDelegations }: { voteData: IVot
 
 	const formatter = new Intl.NumberFormat('en-US', { notation: 'compact' });
 
+	const [visibleDelegators, setVisibleDelegators] = useState(3); // Initially show 3 delegators
+
 	const calculateDelegatedCapital = () => {
 		const totalCapital = new BN(0);
 		voterDelegations.forEach((delegator: IVoteData) => {
@@ -29,6 +31,15 @@ function DelegatedVotesDropdown({ voteData, voterDelegations }: { voteData: IVot
 	const formatBalance = (balance: string) => {
 		return formatter.format(Number(formatBnBalance(balance, { withThousandDelimitor: false }, network)));
 	};
+
+	const handleViewMoreClick = () => {
+		setVisibleDelegators(voterDelegations.length);
+	};
+
+	const handleViewLessClick = () => {
+		setVisibleDelegators(3);
+	};
+
 	return (
 		<div className='mb-2 px-2'>
 			<div className='mb-3 mt-2 flex items-center justify-between'>
@@ -85,22 +96,36 @@ function DelegatedVotesDropdown({ voteData, voterDelegations }: { voteData: IVot
 					<span className='w-1/4 text-xs font-medium text-basic_text dark:text-btn_primary_text'>{t('delegation.delegatorListHeader.conviction')}</span>
 					<span className='text-xs font-medium text-basic_text dark:text-btn_primary_text'>{t('delegation.delegatorListHeader.votingPower')}</span>
 				</div>
-				{voterDelegations.map((delegator: IVoteData) => (
-					<div
-						key={delegator?.voterAddress}
-						className='mt-2.5 w-full space-y-1 border-b border-dashed border-primary_border pb-3'
-					>
-						<div className='flex justify-between font-normal sm:text-xs'>
-							<div className='w-1/2'>
-								<Address address={delegator?.voterAddress} />
+				<div className='max-h-[300px] overflow-y-auto'>
+					{voterDelegations.slice(0, visibleDelegators).map((delegator: IVoteData) => (
+						<div
+							key={delegator?.voterAddress}
+							className='mt-2.5 w-full space-y-1 border-b border-dashed border-primary_border pb-3'
+						>
+							<div className='flex justify-between font-normal sm:text-xs'>
+								<div className='w-1/2'>
+									<Address address={delegator?.voterAddress} />
+								</div>
+								<span className='mx-0 w-1/4 items-start text-basic_text dark:text-btn_primary_text'>{delegator?.lockPeriod || '0'}/d</span>
+								<span className='mx-0 px-0 text-basic_text dark:text-btn_primary_text'>
+									{formatBalance(delegator?.totalVotingPower?.toString() || '0')} {NETWORKS_DETAILS[`${network}`].tokenSymbol}
+								</span>
 							</div>
-							<span className='mx-0 w-1/4 items-start text-basic_text dark:text-btn_primary_text'>{delegator?.lockPeriod || '0'}/d</span>
-							<span className='mx-0 px-0 text-basic_text dark:text-btn_primary_text'>
-								{formatBalance(delegator?.totalVotingPower?.toString() || '0')} {NETWORKS_DETAILS[`${network}`].tokenSymbol}
-							</span>
 						</div>
-					</div>
-				))}
+					))}
+
+					{voterDelegations.length > 3 && (
+						<div className='mt-3 text-right'>
+							<button
+								type='button'
+								onClick={visibleDelegators === 3 ? handleViewMoreClick : handleViewLessClick}
+								className='text-xs text-btn_primary_background'
+							>
+								{visibleDelegators === 3 ? 'View More' : 'View Less'}
+							</button>
+						</div>
+					)}
+				</div>
 			</div>
 		</div>
 	);
