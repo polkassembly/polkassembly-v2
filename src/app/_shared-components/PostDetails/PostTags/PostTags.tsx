ef980a5cc1673db1114408393e697e6b4fef6df6
Vairamuthu <mvairamuthu2003@gmail.com>
2025-04-23 21:54:26 +0530

    feat(PostTags): enhance tag visibility logic to limit displayed tags and show hidden count

--- src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
+++ src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
@@ -1,21 +1,38 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React from 'react';
 import { ITag } from '@/_shared/types';
 import classes from './PostTags.module.scss';
 
-function PostTags({ tags }: { tags: ITag[] }) {
+function getTagValue(tag: ITag | string): string {
+	if (typeof tag === 'string') return tag;
+	const firstProp = Object.values(tag)[0];
+	return typeof firstProp === 'string' ? firstProp : '';
+}
+
+function PostTags({ tags }: { tags: ITag[] | string[] }) {
+	const totalTags = tags.length;
+	const maxVisible = 2;
+
+	let visibleTags = tags;
+	let hiddenCount = 0;
+
+	if (totalTags >= 3) {
+		visibleTags = tags.slice(0, maxVisible);
+		hiddenCount = totalTags - maxVisible;
+	}
+
 	return (
 		<div className='flex items-center gap-x-2'>
-			{tags.map((tag) => (
+			{visibleTags.map((tag) => (
 				<div
-					key={tag.value}
+					key={getTagValue(tag)}
 					className={classes.tag}
 				>
-					{tag.value}
+					{getTagValue(tag)}
 				</div>
 			))}
+			{hiddenCount > 0 && <div className={classes.tag}>+{hiddenCount}</div>}
 		</div>
 	);
 }
