1ee4b90ebc8c7e664b9c922eee431f1cd77eab66
Vairamuthu <mvairamuthu2003@gmail.com>
2025-04-24 13:51:36 +0530

    refactor(PostTags): streamline tag rendering by removing visibility logic and directly displaying all tags

--- src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
+++ src/app/_shared-components/PostDetails/PostTags/PostTags.tsx
@@ -1,37 +1,21 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import React from 'react';
 import { ITag } from '@/_shared/types';
 import classes from './PostTags.module.scss';
 
-function getTagValue(tag: ITag): string {
-	const firstProp = Object.values(tag)[0];
-	return typeof firstProp === 'string' ? firstProp : '';
-}
-
 function PostTags({ tags }: { tags: ITag[] }) {
-	const totalTags = tags.length;
-	const maxVisible = 2;
-
-	let visibleTags = tags;
-	let hiddenCount = 0;
-
-	if (totalTags >= 3) {
-		visibleTags = tags.slice(0, maxVisible);
-		hiddenCount = totalTags - maxVisible;
-	}
-
 	return (
 		<div className='flex items-center gap-x-2'>
-			{visibleTags.map((tag) => (
+			{tags.map((tag) => (
 				<div
-					key={getTagValue(tag)}
+					key={tag.value}
 					className={classes.tag}
 				>
-					{getTagValue(tag)}
+					{tag.value}
 				</div>
 			))}
-			{hiddenCount > 0 && <div className={classes.tag}>+{hiddenCount}</div>}
 		</div>
 	);
 }
