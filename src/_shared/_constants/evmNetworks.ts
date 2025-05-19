44584e137d184f34db65df28fd632b39ee79e192
alphainfinitus <kumar.mridul000@gmail.com>
2025-05-19 22:42:57 +0530

    feat: add EVM networks constants and integrate into SingleComment component
    - Introduced a new constants file for EVM networks to facilitate future network handling.
    - Updated the SingleComment component to utilize the EVM_NETWORKS constant for filtering user addresses based on the current network, enhancing address display logic.

---
+++ src/_shared/_constants/evmNetworks.ts
@@ -0,0 +1,9 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { ENetwork } from '../types';
+
+// TODO: add moonbeam etc
+
+export const EVM_NETWORKS: ENetwork[] = [];
