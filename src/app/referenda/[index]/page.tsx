edc8179b5bf28ac3b9bdb6a955e25f70d09ba2ce
KanishkaRajputd <kanishka6393rajput@gmail.com>
2025-05-20 17:17:36 +0530

    feat: add open graph image support for networks in metadata generation
    - Introduced openGraphImage property in INetworkDetails interface to store large and small image URLs for each network.
    - Updated generateMetadata function to include open graph images for referenda, enhancing social media sharing capabilities with appropriate images for each network.

--- src/app/referenda/[index]/page.tsx
+++ src/app/referenda/[index]/page.tsx
@@ -10,13 +10,19 @@ import { headers } from 'next/headers';
 import { Metadata } from 'next';
 import { OPENGRAPH_METADATA } from '@/_shared/_constants/opengraphMetadata';
 import PollForProposal from '@/app/_shared-components/PollForProposal';
+import { getNetworkFromHeaders } from '@/app/api/_api-utils/getNetworkFromHeaders';
+import { NETWORKS_DETAILS } from '@/_shared/_constants/networks';
 
 export async function generateMetadata({ params }: { params: Promise<{ index: string }> }): Promise<Metadata> {
 	const { index } = await params;
+
+	const network = await getNetworkFromHeaders();
 	const { data } = await NextApiClientService.fetchProposalDetails({ proposalType: EProposalType.REFERENDUM_V2, indexOrHash: index });
 
 	// Default description and title
 	let { description, title } = OPENGRAPH_METADATA;
+	const image = NETWORKS_DETAILS[`${network}`].openGraphImage?.large;
+	const smallImage = NETWORKS_DETAILS[`${network}`].openGraphImage?.small;
 
 	// Use post title in description if available
 	if (data) {
@@ -24,9 +30,41 @@ export async function generateMetadata({ params }: { params: Promise<{ index: st
 		description = `Referendum #${index}: ${data.contentSummary?.postSummary ? data.contentSummary.postSummary : data.title}`;
 	}
 
+	const url = `https://polkassembly.com/referenda/${index}`;
+
 	return {
 		title,
-		description
+		description,
+		metadataBase: new URL('https://polkassembly.com'),
+		icons: [{ url: '/favicon.ico' }],
+		openGraph: {
+			title,
+			description,
+			images: [
+				{
+					url: image || '',
+					width: 1200,
+					height: 630,
+					alt: `Polkassembly Referendum #${index}`
+				},
+				{
+					url: smallImage || '',
+					width: 600,
+					height: 600,
+					alt: `Polkassembly Referendum #${index}`
+				}
+			],
+			siteName: 'Polkassembly',
+			type: 'website',
+			url
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title,
+			description,
+			images: image ? [image] : [],
+			site: '@polkassembly'
+		}
 	};
 }
 
