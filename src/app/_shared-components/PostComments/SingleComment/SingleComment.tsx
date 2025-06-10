017294878854b148c3502e7a54c5937a2647f9f0
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2025-06-10 14:03:29 +0100

    check for window undefined

--- src/app/_shared-components/PostComments/SingleComment/SingleComment.tsx
+++ src/app/_shared-components/PostComments/SingleComment/SingleComment.tsx
@@ -217,7 +217,7 @@ function SingleComment({ commentData, proposalType, index, setParentComment }: S
 	const userAddresses = !EVM_NETWORKS.includes(network) ? comment?.publicUser?.addresses?.filter((address) => !address.startsWith('0x')) : comment?.publicUser?.addresses;
 
 	const addressToDisplay = userAddresses?.[0] || comment?.publicUser?.addresses?.[0];
-	const isHighlighted = window?.location?.hash === `#comment-${comment.id}`;
+	const isHighlighted = typeof window !== 'undefined' && window?.location?.hash === `#comment-${comment.id}`;
 	const wrapperClassName = isHighlighted ? `${classes.wrapper} ${classes.highlighted}` : classes.wrapper;
 
 	return (
