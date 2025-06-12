fcfad5563a3fa19bd84beff43a9f092abd913605
aadarsh012 <aadarsh012@gmail.com>
2025-06-13 01:54:05 +0530

    fix

--- src/app/api/_api-constants/jwt.ts
+++ src/app/api/_api-constants/jwt.ts
@@ -2,18 +2,18 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import { CookieSerializeOptions } from 'cookie';
-import { getSharedEnvVars } from '@/_shared/_utils/getSharedEnvVars';
-import { EAppEnv } from '@/_shared/types';
+// import { getSharedEnvVars } from '@/_shared/_utils/getSharedEnvVars';
+// import { EAppEnv } from '@/_shared/types';
 import { ACCESS_TOKEN_LIFE_IN_SECONDS, REFRESH_TOKEN_LIFE_IN_SECONDS } from './timeConstants';
 
-const { NEXT_PUBLIC_APP_ENV } = getSharedEnvVars();
+// const { NEXT_PUBLIC_APP_ENV } = getSharedEnvVars();
 
 export const REFRESH_TOKEN_COOKIE_OPTIONS: CookieSerializeOptions = {
 	httpOnly: false,
 	maxAge: REFRESH_TOKEN_LIFE_IN_SECONDS,
 	path: '/',
 	sameSite: 'none',
-	secure: NEXT_PUBLIC_APP_ENV === EAppEnv.PRODUCTION
+	secure: true
 };
 
 export const ACCESS_TOKEN_COOKIE_OPTIONS: CookieSerializeOptions = {
@@ -21,5 +21,5 @@ export const ACCESS_TOKEN_COOKIE_OPTIONS: CookieSerializeOptions = {
 	maxAge: ACCESS_TOKEN_LIFE_IN_SECONDS,
 	path: '/',
 	sameSite: 'none',
-	secure: NEXT_PUBLIC_APP_ENV === EAppEnv.PRODUCTION
+	secure: true
 };
