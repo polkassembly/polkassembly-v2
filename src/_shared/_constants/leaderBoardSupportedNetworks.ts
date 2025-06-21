38ee4a0de48eec68e689d0b1e4f98b1924e0c3d9
alphainfinitus <kumar.mridul000@gmail.com>
2025-06-21 14:57:42 +0530

    refactor: reintroduce ENetwork type for LEADERBOARD_SUPPORTED_NETWORKS
    - Updated LEADERBOARD_SUPPORTED_NETWORKS to use the ENetwork type, enhancing type safety and clarity in the codebase.

--- src/_shared/_constants/leaderBoardSupportedNetworks.ts
+++ src/_shared/_constants/leaderBoardSupportedNetworks.ts
@@ -2,4 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const LEADERBOARD_SUPPORTED_NETWORKS = [];
+import { ENetwork } from '../types';
+
+export const LEADERBOARD_SUPPORTED_NETWORKS: ENetwork[] = [];
