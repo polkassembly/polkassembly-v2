26fe923d2a173eab4f4d46fc3f833bdac8890a1d
alphainfinitus <kumar.mridul000@gmail.com>
2025-06-26 23:06:44 +0530

    feat: add lost.gif and integrate into NotFound component
    - Introduced a new GIF asset (lost.gif) to enhance the visual feedback for 404 errors.
    - Updated NotFound component to display the lost.gif using Next.js Image component, improving user experience when a page is not found.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,20 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React from 'react';
+import LostGIF from '@assets/gifs/lost.gif';
+import Image from 'next/image';
 import HomeLinkButton from './_shared-components/HomeLinkButton';
 
 export default function NotFound() {
 	return (
 		<section className='flex min-h-full flex-col items-center justify-center gap-3 rounded-3xl border border-primary_border p-8'>
+			<Image
+				src={LostGIF}
+				alt='404 - Lost'
+				width={200}
+				height={200}
+			/>
+
 			<h2 className='text-xl font-semibold'>Page Not Found</h2>
 			<p className='text-text_secondary py-4'>The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
 			<small>
