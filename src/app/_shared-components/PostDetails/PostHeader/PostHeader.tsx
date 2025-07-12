75eb8a36ccc02d18c7c2ab67ca8cadd12a79e63a
alphainfinitus <kumar.mridul000@gmail.com>
2025-07-12 14:21:00 +0530

    fix: streamline proposalType checks in PostHeader component
    - Simplified conditional rendering by removing unnecessary checks for linkedPost and proposalType, enhancing code clarity.
    - Ensured consistent usage of proposalType in translation function calls for improved readability.

--- src/app/_shared-components/PostDetails/PostHeader/PostHeader.tsx
+++ src/app/_shared-components/PostDetails/PostHeader/PostHeader.tsx
@@ -72,7 +72,8 @@ function PostHeader({ postData, isModalOpen }: { postData: IPost; isModalOpen: b
 				>
 					View All {postData.onChainInfo?.origin ? `${convertCamelCaseToTitleCase(postData.onChainInfo?.origin || '')}` : `${postData.proposalType}`}
 				</Link>
-				{!!postData?.linkedPost && !!postData?.linkedPost?.proposalType && !!postData.proposalType && (
+
+				{postData?.linkedPost?.proposalType && postData.proposalType && (
 					<>
 						<Separator
 							orientation='vertical'
@@ -82,14 +83,14 @@ function PostHeader({ postData, isModalOpen }: { postData: IPost; isModalOpen: b
 							href={getPostDetailsUrl({ proposalType: postData.linkedPost?.proposalType, proposalId: Number(postData.linkedPost?.indexOrHash), network })}
 							className='flex items-center gap-x-1 text-xs text-listing_page_btn hover:underline'
 						>
-							<span className='text-text_secondary'>{t(`PostDetails.ProposalType.${(postData.linkedPost?.proposalType || '').toLowerCase()}`)}</span>
+							<span className='text-text_secondary'>{t(`PostDetails.ProposalType.${postData.linkedPost.proposalType.toLowerCase()}`)}</span>
 							<span className='text-text_primary'>#{postData.linkedPost?.indexOrHash}</span>
 						</Link>
 
 						<ChevronsRight className='h-3.5 w-3.5 text-lg text-basic_text' />
 
 						<div className='flex items-center gap-x-1 text-xs text-text_pink'>
-							<span className='capitalize'>{t(`PostDetails.ProposalType.${(postData.proposalType || '').toLowerCase()}`)}</span>
+							<span className='capitalize'>{t(`PostDetails.ProposalType.${postData.proposalType.toLowerCase()}`)}</span>
 							<span>#{postData?.index}</span>
 						</div>
 					</>
