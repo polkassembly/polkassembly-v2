10aaa71b7adb5f9ddc75047c8c93f49ca69c629c
aadarsh012 <aadarsh012@gmail.com>
2025-07-23 15:57:25 +0530

    mimir detection optimisation

--- src/app/_client-services/isMimirDetected.ts
+++ src/app/_client-services/isMimirDetected.ts
@@ -5,13 +5,13 @@
 import { isMimirReady, MIMIR_REGEXP } from '@mimirdev/apps-inject';
 
 export const isMimirDetected = async () => {
-	if (typeof window === 'undefined') {
-		return false;
-	}
+	if (typeof window === 'undefined') return false;
 
 	const isInIframe = window !== window.parent;
 
+	if (!isInIframe) return false;
+
 	const origin = await isMimirReady();
 
-	return isInIframe && origin && MIMIR_REGEXP.test(origin);
+	return origin && MIMIR_REGEXP.test(origin);
 };
