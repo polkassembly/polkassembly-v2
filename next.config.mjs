6a7e92fed7647d9c70777c48633ddd4816013c63
alphainfinitus <kumar.mridul000@gmail.com>
2025-09-03 13:43:36 +0530

    Update Permissions-Policy in next.config.mjs to restrict camera and geolocation access for enhanced security

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = {
 					{ key: 'X-XSS-Protection', value: '1; mode=block' },
 					{ key: 'X-Content-Type-Options', value: 'nosniff' },
 					{ key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-					{ key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+					{ key: 'Permissions-Policy', value: 'camera=self, microphone=(), geolocation=self' },
 					{
 						key: 'Content-Security-Policy',
 						value: [
