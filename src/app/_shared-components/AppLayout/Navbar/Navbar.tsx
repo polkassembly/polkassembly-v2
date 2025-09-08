d690e3a161db6033e8570d970a2815c22e22f84e
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2025-09-08 20:49:55 +0100

    update color

--- src/app/_shared-components/AppLayout/Navbar/Navbar.tsx
+++ src/app/_shared-components/AppLayout/Navbar/Navbar.tsx
@@ -204,7 +204,7 @@ function Navbar() {
 										className={classes.dropdownMenuContent}
 										href={`/set-identity?open=${ESetIdentityStep.CLEAR_IDENTITY}`}
 									>
-										<ShieldMinusIcon className='h-6 w-6 font-light text-text_primary opacity-80' />
+										<ShieldMinusIcon className='h-6 w-6 font-light text-basic_text' />
 										{t('SetIdentity.clearIdentity')}
 									</Link>
 								</DropdownMenuItem>
