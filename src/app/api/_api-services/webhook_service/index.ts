91e79c1c33b3f69a72e91e4e19f2cd95cae336e0
AleemAlam <aleem.alam@outlook.com>
2025-09-09 15:23:58 +0200

    Refactor user votes deletion logic in WebhookService to conditionally execute based on address presence

--- src/app/api/_api-services/webhook_service/index.ts
+++ src/app/api/_api-services/webhook_service/index.ts
@@ -251,7 +251,7 @@ export class WebhookService {
 			RedisService.DeleteActivityFeed({ network }),
 			RedisService.DeleteAllSubscriptionFeedsForNetwork(network),
 			RedisService.DeleteOverviewPageData({ network }),
-			RedisService.DeleteUserVotesByAddress({ network, address }),
+			...(address ? [RedisService.DeleteUserVotesByAddress({ network, address })] : []),
 			RedisService.DeleteTrackCounts({ network })
 		]);
 
