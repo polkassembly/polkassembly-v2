5e1d8d5445b6e19dfca5de664c597bee4b659855
Vairamuthu <mvairamuthu2003@gmail.com>
2025-10-01 13:32:45 +0530

    feat: refactor notification handling and consolidate icon and label functions in NotificationConstants

--- src/app/_shared-components/Profile/Settings/Notifications/Notifications.tsx
+++ src/app/_shared-components/Profile/Settings/Notifications/Notifications.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-'use client';
-
 import { getCurrentNetwork } from '@/_shared/_utils/getCurrentNetwork';
 import NotificationChannelsSection from './sections/NotificationChannelsSection';
 import ParachainsSection from './sections/ParachainsSection';
