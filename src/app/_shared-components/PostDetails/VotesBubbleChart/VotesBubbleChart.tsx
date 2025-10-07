22c54448a685e93e8887780eb1319c6528186097
Mridul Kumar <kumar.mridul000@gmail.com>
2025-10-07 13:36:00 +0530

    Update VotesBubbleChart.tsx

--- src/app/_shared-components/PostDetails/VotesBubbleChart/VotesBubbleChart.tsx
+++ src/app/_shared-components/PostDetails/VotesBubbleChart/VotesBubbleChart.tsx
@@ -129,7 +129,7 @@ const getPostAnalytics = async ({
 		analyticsType,
 		votesType
 	});
-	if (error && (!data || data === null)) {
+	if (error) {
 		throw new Error(error?.message || 'Failed to fetch data');
 	}
 	return data;
