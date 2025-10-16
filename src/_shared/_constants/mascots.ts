ca1a41869f938ebd8429068b14c23adaff2bd331
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2025-10-16 17:26:51 +0100

    update naming

--- src/_shared/_constants/mascots.ts
+++ src/_shared/_constants/mascots.ts
@@ -1,9 +1,9 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import Error from '@assets/klara/error.svg';
-import Loading from '@assets/klara/klara-thinking.svg';
-import Welcome from '@assets/klara/klara-welcome.svg';
+import ErrorMascot from '@assets/klara/error.svg';
+import LoadingMascot from '@assets/klara/klara-thinking.svg';
+import WelcomeMascot from '@assets/klara/klara-welcome.svg';
 
 export interface MascotGif {
 	type: 'error' | 'loading' | 'welcome';
@@ -13,14 +13,14 @@ export interface MascotGif {
 export const mascotGifs: MascotGif[] = [
 	{
 		type: 'error',
-		url: Error
+		url: ErrorMascot
 	},
 	{
 		type: 'loading',
-		url: Loading
+		url: LoadingMascot
 	},
 	{
 		type: 'welcome',
-		url: Welcome
+		url: WelcomeMascot
 	}
 ];
