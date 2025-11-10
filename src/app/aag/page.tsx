fb8be51f9374a3e2d78eef01357f2d199c0e99a6
Vairamuthu <mvairamuthu2003@gmail.com>
2025-11-10 19:43:02 +0530

    feat: add AAG component for governance insights and calendar integration

---
+++ src/app/aag/page.tsx
@@ -0,0 +1,25 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { Button } from '../_shared-components/Button';
+
+function AAG() {
+	return (
+		<div>
+			<div>
+				<div className='flex'>
+					<p>Attempts At Governance (AAG) </p>
+					<Button>Add to Calendar</Button>
+				</div>
+				<p>Host: The KUS DAO</p>
+				<div>
+					<p>The #1 news resource on Polkadot. Daily news, insights & interviews with the top minds in the Polkadot ecosystem.</p>
+					<p>Request to Present</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default AAG;
