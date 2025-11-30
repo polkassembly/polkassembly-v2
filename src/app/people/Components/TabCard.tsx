13f2b01c63fe3b690d528103f578555087531e97
Vairamuthu <88650559+vmmuthu31@users.noreply.github.com>
2025-12-01 00:39:04 +0530

    Update TabCard.tsx

--- src/app/people/Components/TabCard.tsx
+++ src/app/people/Components/TabCard.tsx
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+'use client'
+
 import { useTranslations } from 'next-intl';
 import { TabsList, TabsTrigger } from '@/app/_shared-components/Tabs';
 
