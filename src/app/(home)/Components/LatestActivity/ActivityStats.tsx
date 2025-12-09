ed81c0c60f5e4e84ec17743c64da8410243fea75
Vairamuthu <mvairamuthu2003@gmail.com>
2025-12-09 14:08:29 +0530

    feat: Add an OpenGov sidebar item with a new icon and a tooltip for total spends in activity stats, along with corresponding internationalization updates.

--- src/app/(home)/Components/LatestActivity/ActivityStats.tsx
+++ src/app/(home)/Components/LatestActivity/ActivityStats.tsx
@@ -8,6 +8,8 @@ import tokens from '@assets/delegation/tokens.svg';
 import Image from 'next/image';
 import { BN, BN_ZERO } from '@polkadot/util';
 import { useQuery } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
+import { AiFillQuestionCircle } from '@react-icons/all-files/ai/AiFillQuestionCircle';
 import { formatBnBalance } from '@/app/_client-utils/formatBnBalance';
 import { getCurrentNetwork } from '@/_shared/_utils/getCurrentNetwork';
 import { ENetwork } from '@/_shared/types';
@@ -15,7 +17,7 @@ import { Skeleton } from '@/app/_shared-components/Skeleton';
 import { NextApiClientService } from '@/app/_client-services/next_api_client_service';
 import { ClientError } from '@/app/_client-utils/clientError';
 import { FIVE_MIN_IN_MILLI } from '@/app/api/_api-constants/timeConstants';
-import { useTranslations } from 'next-intl';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/app/_shared-components/Tooltip';
 
 function ActivityStats() {
 	const t = useTranslations('Overview');
@@ -60,7 +62,8 @@ function ActivityStats() {
 		{
 			title: t('totalSpends'),
 			value: formattedSpends,
-			icon: tokens
+			icon: tokens,
+			tooltip: t('totalSpendsTooltip')
 		}
 	];
 
@@ -81,7 +84,19 @@ function ActivityStats() {
 						/>
 					</div>
 					<div className='flex flex-col'>
-						<span className='text-xs font-medium text-wallet_btn_text'>{stat.title}</span>
+						<div className='flex items-center gap-1'>
+							<span className='text-xs font-medium text-wallet_btn_text'>{stat.title}</span>
+							{stat.tooltip && (
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<AiFillQuestionCircle className='h-3 w-3 text-question_icon_color' />
+									</TooltipTrigger>
+									<TooltipContent className='w-40 break-words bg-tooltip_background p-2 text-center text-xs text-white'>
+										<p>{stat.tooltip}</p>
+									</TooltipContent>
+								</Tooltip>
+							)}
+						</div>
 						<div className='flex items-end gap-2'>
 							{isFetching ? <Skeleton className='h-6 w-20' /> : <span className='text-xl font-semibold text-text_primary'>{stat.value}</span>}
 						</div>
