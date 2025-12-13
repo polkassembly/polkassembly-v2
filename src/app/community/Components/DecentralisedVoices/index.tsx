7cd57e19c8d21a670e257518b891e99746b73c43
Vairamuthu <mvairamuthu2003@gmail.com>
2025-12-13 14:07:27 +0530

    refactor: Mark DecentralisedVoices component as client component.

--- src/app/community/Components/DecentralisedVoices/index.tsx
+++ src/app/community/Components/DecentralisedVoices/index.tsx
@@ -1,6 +1,9 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+
+'use client';
+
 import { FaFilter } from '@react-icons/all-files/fa/FaFilter';
 import { Popover, PopoverContent, PopoverTrigger } from '@/app/_shared-components/Popover/Popover';
 import { useTranslations } from 'next-intl';
