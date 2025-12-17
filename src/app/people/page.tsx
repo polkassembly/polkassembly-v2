e6834484e0ebad1e15fe388b09673e8f2e8e75f5
Vairamuthu <mvairamuthu2003@gmail.com>
2025-12-17 18:34:08 +0530

    feat: Add community page with delegates listing and metadata.

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import Header from '@/app/community/Components/Header/Header';
+import Header from '@/app/people/Components/Header/Header';
 import { Metadata } from 'next';
 import { OPENGRAPH_METADATA } from '@/_shared/_constants/opengraphMetadata';
 import { getNetworkFromHeaders } from '@/app/api/_api-utils/getNetworkFromHeaders';
